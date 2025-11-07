24ab6ab98982071bdfb30d18ff43da8f4e10d4fe
ruslan <r.stupa@ya.ru>
2025-11-07 15:30:45 +0700

    Fix classnames import casing x Build failed in 180ms error during build: [vite]: Rollup failed to resolve import "classNames" from "/home/runner/work/odnoi-krovi-app/odnoi-krovi-app/frontend/src/pages/registration/index.tsx". This is most likely unintended because it can break your application at runtime.

--- frontend/src/pages/registration/index.tsx
+++ frontend/src/pages/registration/index.tsx
@@ -1,6 +1,6 @@
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-import cn from 'classNames';
+import cn from 'classnames';
 import { ChangeEvent, FC, useState } from 'react';
 import InputMask from 'react-input-mask';
 import { useNavigate } from 'react-router';
@@ -13,185 +13,185 @@ import Layout from 'components/Layout';
 import styles from './Registration.module.less';
 
 type Props = {
-    user: TelegramUser;
+  user: TelegramUser;
 };
 
 type Input = {
-    value: string;
-    isError?: boolean;
+  value: string;
+  isError?: boolean;
 };
 
 const docsLink = 'https://однойкрови.рф/docs';
 const emailRegexp = /^\w+([+.-]?\w+)*@\w+([.-]?\w+)*(\.\w+)+$/i;
 
 const Registration: FC<Props> = ({ user }) => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const [phone, setPhone] = useState<Input>({ value: '' });
-    const [email, setEmail] = useState<Input>({ value: '' });
-    const [name, setName] = useState<Input>({ value: user.fullName });
+  const [phone, setPhone] = useState<Input>({ value: '' });
+  const [email, setEmail] = useState<Input>({ value: '' });
+  const [name, setName] = useState<Input>({ value: user.fullName });
 
-    const isValidEmail = () => email.value.match(emailRegexp);
+  const isValidEmail = () => email.value.match(emailRegexp);
 
-    const onChangeInput =
-        (inputName?: string) =>
-        ({ target: { value } }: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
-            switch (true) {
-                case inputName === 'phone': {
-                    setPhone({ value });
+  const onChangeInput =
+    (inputName?: string) =>
+    ({ target: { value } }: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+      switch (true) {
+        case inputName === 'phone': {
+          setPhone({ value });
 
-                    return;
-                }
-                case inputName === 'email': {
-                    setEmail({ value });
-
-                    return;
-                }
-                default: {
-                    setName({ value });
-                }
-            }
-        };
-
-    const onBlurEmailHandler = () => {
-        if (!isValidEmail()) {
-            setEmail((prevState) => ({ ...prevState, isError: true }));
-
-            toast.warn('Неверный формат e-mail адреса');
+          return;
         }
+        case inputName === 'email': {
+          setEmail({ value });
+
+          return;
+        }
+        default: {
+          setName({ value });
+        }
+      }
     };
 
-    const onConfirmClickHandler = async () => {
-        let isInvalidValue = false;
+  const onBlurEmailHandler = () => {
+    if (!isValidEmail()) {
+      setEmail((prevState) => ({ ...prevState, isError: true }));
 
-        if (!name.value) {
-            isInvalidValue = true;
+      toast.warn('Неверный формат e-mail адреса');
+    }
+  };
 
-            setName({ isError: true, value: '' });
-        }
+  const onConfirmClickHandler = async () => {
+    let isInvalidValue = false;
 
-        if (!phone.value) {
-            isInvalidValue = true;
+    if (!name.value) {
+      isInvalidValue = true;
 
-            setPhone({ isError: true, value: '' });
-        }
+      setName({ isError: true, value: '' });
+    }
 
-        if (phone.value && /_/.test(phone.value)) {
-            isInvalidValue = true;
+    if (!phone.value) {
+      isInvalidValue = true;
 
-            setPhone(({ value }) => ({ isError: true, value }));
+      setPhone({ isError: true, value: '' });
+    }
 
-            toast.warn('Неверный формат номера телефона');
-        }
+    if (phone.value && /_/.test(phone.value)) {
+      isInvalidValue = true;
 
-        if (!email.value) {
-            isInvalidValue = true;
+      setPhone(({ value }) => ({ isError: true, value }));
 
-            setEmail({ isError: true, value: '' });
-        }
+      toast.warn('Неверный формат номера телефона');
+    }
 
-        if (email.value && !isValidEmail()) {
-            isInvalidValue = true;
+    if (!email.value) {
+      isInvalidValue = true;
 
-            toast.warn('Неверный формат e-mail адреса');
-        }
+      setEmail({ isError: true, value: '' });
+    }
 
-        if (isInvalidValue) {
-            return;
-        }
+    if (email.value && !isValidEmail()) {
+      isInvalidValue = true;
 
-        const { data, error } = await updateUser({
-            id: user.id,
-            phone: phone.value,
-            email: email.value,
-            fullName: name.value,
-        });
+      toast.warn('Неверный формат e-mail адреса');
+    }
 
-        if (!data || error) {
-            toast.warn(error); // TODO ?
+    if (isInvalidValue) {
+      return;
+    }
 
-            return;
-        }
+    const { data, error } = await updateUser({
+      id: user.id,
+      phone: phone.value,
+      email: email.value,
+      fullName: name.value,
+    });
 
-        navigate('/owner');
-    };
+    if (!data || error) {
+      toast.warn(error); // TODO ?
 
-    return (
-        <Layout>
-            <div className={styles.header}>
-                <h2 className={styles.title}>Добро пожаловать,</h2>
-                <h4 className={styles.subTitle}>{user.fullName}</h4>
-            </div>
-            <div className={styles.form}>
-                <TextField
-                    fullWidth
-                    variant='standard'
-                    value={name.value}
-                    onChange={onChangeInput()}
-                    placeholder='Как вас зовут?'
-                    slotProps={{
-                        htmlInput: { className: cn(styles.input, { [styles.inputError]: name.isError } as any) },
-                        input: {
-                            className: cn(styles.inputWrapper, { [styles.inputWrapperError]: name.isError } as any),
-                        },
-                    }}
-                />
-                <InputMask mask='+79999999999' value={phone.value} onChange={onChangeInput('phone')}>
-                    {(inputProps) => (
-                        <TextField
-                            {...inputProps}
-                            fullWidth
-                            type='tel'
-                            placeholder='+7'
-                            variant='standard'
-                            className={styles.textField}
-                            slotProps={{
-                                htmlInput: {
-                                    className: cn(styles.input, { [styles.inputError]: phone.isError } as any),
-                                },
-                                input: {
-                                    className: cn(styles.inputWrapper, {
-                                        [styles.inputWrapperError]: phone.isError,
-                                    } as any),
-                                },
-                            }}
-                        />
-                    )}
-                </InputMask>
-                <TextField
-                    fullWidth
-                    variant='standard'
-                    value={email.value}
-                    placeholder='E-mail'
-                    onBlur={onBlurEmailHandler}
-                    onChange={onChangeInput('email')}
-                    slotProps={{
-                        htmlInput: { className: cn(styles.input, { [styles.inputError]: email.isError } as any) },
-                        input: {
-                            className: cn(styles.inputWrapper, {
-                                [styles.inputWrapperError]: email.isError,
-                            } as any),
-                        },
-                    }}
-                />
-                <Button fullWidth variant='contained' className={styles.button} onClick={onConfirmClickHandler}>
-                    Продолжить
-                </Button>
-                <div className={styles.links}>
-                    <p className={styles.agreementLabel}>
-                        Продолжая, Вы принимаете{' '}
-                        <a className={styles.agreementLabelLink} href={docsLink} target='_blank' rel='noreferrer'>
-                            Пользовательское соглашение
-                        </a>{' '}
-                        и{' '}
-                        <a className={styles.agreementLabelLink} href={docsLink} target='_blank' rel='noreferrer'>
-                            Политику конфиденциальности
-                        </a>
-                    </p>
-                </div>
-            </div>
-        </Layout>
-    );
+      return;
+    }
+
+    navigate('/owner');
+  };
+
+  return (
+    <Layout>
+      <div className={styles.header}>
+        <h2 className={styles.title}>Добро пожаловать,</h2>
+        <h4 className={styles.subTitle}>{user.fullName}</h4>
+      </div>
+      <div className={styles.form}>
+        <TextField
+          fullWidth
+          variant='standard'
+          value={name.value}
+          onChange={onChangeInput()}
+          placeholder='Как вас зовут?'
+          slotProps={{
+            htmlInput: { className: cn(styles.input, { [styles.inputError]: name.isError } as any) },
+            input: {
+              className: cn(styles.inputWrapper, { [styles.inputWrapperError]: name.isError } as any),
+            },
+          }}
+        />
+        <InputMask mask='+79999999999' value={phone.value} onChange={onChangeInput('phone')}>
+          {(inputProps) => (
+            <TextField
+              {...inputProps}
+              fullWidth
+              type='tel'
+              placeholder='+7'
+              variant='standard'
+              className={styles.textField}
+              slotProps={{
+                htmlInput: {
+                  className: cn(styles.input, { [styles.inputError]: phone.isError } as any),
+                },
+                input: {
+                  className: cn(styles.inputWrapper, {
+                    [styles.inputWrapperError]: phone.isError,
+                  } as any),
+                },
+              }}
+            />
+          )}
+        </InputMask>
+        <TextField
+          fullWidth
+          variant='standard'
+          value={email.value}
+          placeholder='E-mail'
+          onBlur={onBlurEmailHandler}
+          onChange={onChangeInput('email')}
+          slotProps={{
+            htmlInput: { className: cn(styles.input, { [styles.inputError]: email.isError } as any) },
+            input: {
+              className: cn(styles.inputWrapper, {
+                [styles.inputWrapperError]: email.isError,
+              } as any),
+            },
+          }}
+        />
+        <Button fullWidth variant='contained' className={styles.button} onClick={onConfirmClickHandler}>
+          Продолжить
+        </Button>
+        <div className={styles.links}>
+          <p className={styles.agreementLabel}>
+            Продолжая, Вы принимаете{' '}
+            <a className={styles.agreementLabelLink} href={docsLink} target='_blank' rel='noreferrer'>
+              Пользовательское соглашение
+            </a>{' '}
+            и{' '}
+            <a className={styles.agreementLabelLink} href={docsLink} target='_blank' rel='noreferrer'>
+              Политику конфиденциальности
+            </a>
+          </p>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Registration;
