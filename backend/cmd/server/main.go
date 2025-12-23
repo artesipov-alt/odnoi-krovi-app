d06a55bd0b26160849b9e106e2d0317ae3efd19e
ruslan <r.stupa@ya.ru>
2025-12-23 22:43:30 +0700

    Update API version to 1.2.4 in Swagger documentation

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -32,7 +32,7 @@ import (
 )
 
 // @title 1krovi.app
-// @version 1.2.3
+// @version 1.2.4
 // @description API сервиса однойкрови.рф для донороcства крови и помощи животным
 // @host
 // @BasePath /api/v1
