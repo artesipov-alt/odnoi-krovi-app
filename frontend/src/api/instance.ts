6961fc24597016a13417773b94caee7e8d2fcb81
ruslan <r.stupa@ya.ru>
2026-01-12 23:03:49 +0700

    Update API baseURL to production endpoint

--- frontend/src/api/instance.ts
+++ frontend/src/api/instance.ts
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export const instance = axios.create({
   timeout: 20000,
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'http://1krovi.app/api',
   // headers: { 'Content-Type': 'application/json', 'X-Telegram-WebApp': 'true' },
 });
