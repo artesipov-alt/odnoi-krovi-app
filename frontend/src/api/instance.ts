42d94f256fd0582da24896710adbec12f864bb68
ruslan <r.stupa@ya.ru>
2026-01-12 21:41:42 +0700

    Update API base URL and endpoint paths to use `/v1` prefix

--- frontend/src/api/instance.ts
+++ frontend/src/api/instance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-    timeout: 20000,
-    baseURL: 'https://1krovi.app/api/v1/',
-    // headers: { 'Content-Type': 'application/json', 'X-Telegram-WebApp': 'true' },
+  timeout: 20000,
+  baseURL: 'http://localhost:3001/api',
+  // headers: { 'Content-Type': 'application/json', 'X-Telegram-WebApp': 'true' },
 });
--- frontend/src/api/reference.ts
+++ frontend/src/api/reference.ts
@@ -4,51 +4,51 @@ import { instance } from './instance';
 import { PetType } from './types';
 
 export type PetDict = {
-    value: PetType;
-    label: string;
+  value: PetType;
+  label: string;
 };
 
 export type Dict = {
-    value: number;
-    label: string;
+  value: number;
+  label: string;
 };
 
 export type GetPetsTypesResponse = {
-    data: PetDict[];
+  data: PetDict[];
 };
 
 export type GetBloodGroupsResponse = {
-    data: Dict[];
+  data: Dict[];
 };
 
 export type GetBloodComponentsResponse = {
-    data: Dict[];
+  data: Dict[];
 };
 
 export type GetLocationsResponse = {
-    data: Dict[];
+  data: Dict[];
 };
 
 export interface IReferenceApi {
-    getPetsTypes(): AxiosPromise<GetPetsTypesResponse>;
-    getBloodGroups(pet: PetType): AxiosPromise<GetBloodGroupsResponse>;
-    getBloodComponents(): AxiosPromise<GetBloodComponentsResponse>;
-    getLocations(): AxiosPromise<GetLocationsResponse>;
+  getPetsTypes(): AxiosPromise<GetPetsTypesResponse>;
+  getBloodGroups(pet: PetType): AxiosPromise<GetBloodGroupsResponse>;
+  getBloodComponents(): AxiosPromise<GetBloodComponentsResponse>;
+  getLocations(): AxiosPromise<GetLocationsResponse>;
 }
 
-export const REFERENCE_URL = 'reference/';
+export const REFERENCE_URL = '/v1/reference';
 
 export const referenceApi = (): IReferenceApi => ({
-    getPetsTypes() {
-        return instance.get(`${REFERENCE_URL}pet-types/`);
-    },
-    getBloodGroups(pet) {
-        return instance.get(`${REFERENCE_URL}blood-groups/${pet}`);
-    },
-    getBloodComponents() {
-        return instance.get(`${REFERENCE_URL}blood-components/`);
-    },
-    getLocations() {
-        return instance.get(`${REFERENCE_URL}locations/`);
-    },
+  getPetsTypes() {
+    return instance.get(`${REFERENCE_URL}/pet-types`);
+  },
+  getBloodGroups(pet) {
+    return instance.get(`${REFERENCE_URL}/blood-groups/${pet}`);
+  },
+  getBloodComponents() {
+    return instance.get(`${REFERENCE_URL}/blood-components`);
+  },
+  getLocations() {
+    return instance.get(`${REFERENCE_URL}/locations`);
+  },
 });
