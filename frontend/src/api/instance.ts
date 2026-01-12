3fa003d5303e3d496f138f7a597b2d4a7ff5fc32
ruslan <r.stupa@ya.ru>
2026-01-12 23:54:55 +0700

    Update API base URL to use HTTPS protocol

--- frontend/src/api/instance.ts
+++ frontend/src/api/instance.ts
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export const instance = axios.create({
   timeout: 20000,
-  baseURL: 'http://1krovi.app/api',
+  baseURL: 'https://1krovi.app/api',
   // headers: { 'Content-Type': 'application/json', 'X-Telegram-WebApp': 'true' },
 });
