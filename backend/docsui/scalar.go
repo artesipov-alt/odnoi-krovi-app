85341ebf2c48420261fddaaf296bf9d3d0288247
ruslan <r.stupa@ya.ru>
2026-01-14 01:12:54 +0700

    Fix indentation

--- backend/docsui/scalar.go
+++ backend/docsui/scalar.go
@@ -8,26 +8,26 @@ import (
 func ScalarDocsHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	w.Write([]byte(`
-    <!doctype html>
-    <html>
-      <head>
-        <title>API Documentation</title>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </head>
-      <body>
-        <script
-          id="api-reference"
-          data-url="/api/openapi.json"
-          data-configuration='{
-            "theme": "fastify",
-            "layout": "classic",
-            "hideClientButton": true,
-            "defaultServerUrl": "/api",
-          }'>
-        </script>
-        <script src="https://cdn.jsdelivr.net/npm/@scalar/api-reference"></script>
-      </body>
-    </html>
-    `))
+				<!doctype html>
+				<html>
+						<head>
+								<title>API Documentation</title>
+								<meta charset="utf-8" />
+								<meta name="viewport" content="width=device-width, initial-scale=1" />
+						</head>
+						<body>
+								<script
+										id="api-reference"
+										data-url="/api/openapi.json"
+										data-configuration='{
+												"theme": "fastify",
+												"layout": "classic",
+												"hideClientButton": true,
+												"defaultServerUrl": "/api"
+										}'>
+								</script>
+								<script src="https://cdn.jsdelivr.net/npm/@scalar/api-reference"></script>
+						</body>
+				</html>
+				`))
 }
