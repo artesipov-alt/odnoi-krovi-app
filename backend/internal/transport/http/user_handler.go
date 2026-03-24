0b2d47d82481ce5d740597280b82bab0b8052ef1
ruslan <r.stupa@ya.ru>
2026-03-24 15:15:33 +0300

    refactor(dto): Add identities field to UserDetail struct and remove debug log

--- backend/internal/transport/http/user_handler.go
+++ backend/internal/transport/http/user_handler.go
@@ -114,8 +114,6 @@ func (h *UserHandler) Register(api huma.API) {
 // Handlers
 
 func (h *UserHandler) GetUser(ctx context.Context, input *dto.GetUserByIDInput) (*dto.GetUserByIDOutput, error) {
-	slog.DebugContext(ctx, "getting user", "user_id", input.ID)
-
 	usr, err := h.getByIDHandler.Handle(ctx, input.ID, input.WithPets, input.WithDonorPreference)
 	if err != nil {
 		return nil, err
