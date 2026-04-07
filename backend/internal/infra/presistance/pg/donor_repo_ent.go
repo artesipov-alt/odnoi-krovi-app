c7161dbb487ffd572eb598345dea8e5b2508fa8c
ruslan <r.stupa@ya.ru>
2026-04-07 19:45:52 +0300

    fix(bloodreq): include unconfirmed donor responses

--- backend/internal/infra/presistance/pg/donor_repo_ent.go
+++ backend/internal/infra/presistance/pg/donor_repo_ent.go
@@ -117,7 +117,7 @@ func (r *EntDonorResponseRepository) GetRecipient(ctx context.Context, id string
 
 func (r *EntDonorResponseRepository) GetByPetID(ctx context.Context, petID string) (*donormodel.DonorResponse, error) {
 	entResp, err := r.client(ctx).DonorResponse.Query().
-		Where(donorresponse.HasDonorWith(pet.ID(petID)), donorresponse.StatusNEQ("completed")).
+		Where(donorresponse.HasDonorWith(pet.ID(petID)), donorresponse.Or(donorresponse.StatusNEQ(donorresponse.StatusCompleted), donorresponse.IsConfirmedNEQ(true))).
 		WithRequest(func(q *ent.BloodSearchRequestQuery) { q.Select(bloodsearchrequest.FieldID) }).
 		WithDonor(func(q *ent.PetQuery) { q.Select(pet.FieldID) }).
 		Only(ctx)
