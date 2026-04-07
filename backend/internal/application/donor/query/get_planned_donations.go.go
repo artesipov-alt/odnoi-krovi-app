2953672d5fb1fc9c8e783e82fd75c1ccc47adf86
ruslan <r.stupa@ya.ru>
2026-04-07 19:45:14 +0300

    feture(donor): Add planned donations query handler

---
+++ backend/internal/application/donor/query/get_planned_donations.go.go
@@ -0,0 +1,75 @@
+package query
+
+import (
+	"context"
+	"errors"
+
+	"github.com/artesipov-alt/odnoi-krovi-app/internal/apperrors"
+	"github.com/artesipov-alt/odnoi-krovi-app/internal/domain/bloodsearch"
+	bloodreqmodel "github.com/artesipov-alt/odnoi-krovi-app/internal/domain/bloodsearch/model"
+	"github.com/artesipov-alt/odnoi-krovi-app/internal/domain/donor"
+	donormodel "github.com/artesipov-alt/odnoi-krovi-app/internal/domain/donor/model"
+	"github.com/artesipov-alt/odnoi-krovi-app/internal/domain/pet"
+	"github.com/artesipov-alt/odnoi-krovi-app/internal/domain/user"
+	usermodel "github.com/artesipov-alt/odnoi-krovi-app/internal/domain/user/model"
+)
+
+type GetPlannedDonationsResult struct {
+	ApplicationData   donormodel.DonorResponse
+	RecipientData     bloodreqmodel.BloodRequestWithApplications
+	RecipentOwnerData usermodel.User
+}
+
+type PlannedDonationsHandler struct {
+	donorRepo    donor.Repository
+	petRepo      pet.Repository
+	bloodReqRepo bloodsearch.BloodRequestRepository
+	userRepo     user.Repository
+}
+
+func NewPlannedDonationsHandler(donorRepo donor.Repository, petRepo pet.Repository, bloodReqRepo bloodsearch.BloodRequestRepository, userRepo user.Repository) *PlannedDonationsHandler {
+	return &PlannedDonationsHandler{
+		donorRepo:    donorRepo,
+		petRepo:      petRepo,
+		bloodReqRepo: bloodReqRepo,
+		userRepo:     userRepo,
+	}
+}
+
+func (h *PlannedDonationsHandler) Handle(ctx context.Context, userID string) ([]*GetPlannedDonationsResult, error) {
+	donorPets, err := h.petRepo.GetByUserID(ctx, userID, pet.PetPreloadOptions{
+		WithAll: true,
+	})
+	if err != nil {
+		return nil, apperrors.Internal(err, "failed to get pets for user")
+	}
+
+	result := make([]*GetPlannedDonationsResult, len(donorPets))
+	for _, dPet := range donorPets {
+		application, err := h.donorRepo.GetByPetID(ctx, dPet.ID)
+		if err != nil && !errors.Is(err, apperrors.ErrDonorResponseNotFound) {
+			return nil, apperrors.Internal(err, "failed to get donor application")
+		}
+		if application != nil {
+			request, err := h.bloodReqRepo.GetByApplicationID(ctx, application.ID)
+			if err != nil && !errors.Is(err, apperrors.ErrBloodRequestNotFound) {
+				return nil, apperrors.Internal(err, "failed to get blood request")
+			}
+			recipientPet, err := h.petRepo.GetByID(ctx, request.PetID, pet.PetPreloadOptions{})
+			if err != nil {
+				return nil, apperrors.Internal(err, "failed to get pet")
+			}
+			user, err := h.userRepo.GetByID(ctx, recipientPet.OwnerID, user.UserPreloadOptions{})
+			if err != nil {
+				return nil, apperrors.Internal(err, "failed to get user")
+			}
+			result = append(result, &GetPlannedDonationsResult{
+				ApplicationData:   *application,
+				RecipientData:     *request,
+				RecipentOwnerData: *user,
+			})
+		}
+	}
+
+	return result, nil
+}
