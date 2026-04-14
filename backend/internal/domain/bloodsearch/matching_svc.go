ece6fab49d8d9e0990f5e752ec61456cabe67b21
ruslan <r.stupa@ya.ru>
2026-04-14 23:55:45 +0300

    hotfix(svc): remove region matching from donor matching logic
    The region matching logic was commented out and then removed from the
    final `if` condition. This simplifies the matching process by
    disregarding regional preferences.

--- backend/internal/domain/bloodsearch/matching_svc.go
+++ backend/internal/domain/bloodsearch/matching_svc.go
@@ -25,17 +25,17 @@ func hasIntersection(a, b []string) bool {
 func (r *MatchingService) MatchDonor(bloodreq *bloodreqmodel.BloodRequestWithMatchingDonors, pet *petmodel.Pet, preferredLocations []string) {
 	sameBlood := false
 	sameType := false
-	sameRegion := false
+	// sameRegion := false
 	coversNeededAmount := false
 	avilableDonorAmount := pet.CalculateDonationAmount()
 	halfVolume := (bloodreq.BloodVolumeNeeded - bloodreq.BloodVolumeReserved) / 2
-	bloodSearchRegions := bloodreq.Regions
+	// bloodSearchRegions := bloodreq.Regions
 
 	// (Тип-питомца) Бизнес-логика, типы питомцев должны совпадать
 	sameType = pet.Type == bloodreq.RecipientData.PetType
 
 	// (Регионы) Бизнес-логика, если запрос не указал регионы или донор не указал предпочтений, подходит; иначе - пересечение регионов
-	sameRegion = len(bloodSearchRegions) == 0 || len(preferredLocations) == 0 || hasIntersection(preferredLocations, bloodSearchRegions)
+	// sameRegion = len(bloodSearchRegions) == 0 || len(preferredLocations) == 0 || hasIntersection(preferredLocations, bloodSearchRegions)
 
 	// (Группа-крови) Бизнес-логика, должна быть та же группа крови или неизвестная если реципиент разрешил
 	sameBlood = slices.Contains(bloodreq.BloodGroupNames, pet.BloodGroupName) || (bloodreq.IncludeUnknownBloodGroup && pet.BloodGroupName == "UNKNOWN")
@@ -47,7 +47,7 @@ func (r *MatchingService) MatchDonor(bloodreq *bloodreqmodel.BloodRequestWithMat
 		coversNeededAmount = true
 	}
 
-	if sameType && sameRegion && sameBlood && coversNeededAmount {
+	if sameType && sameBlood && coversNeededAmount {
 		donorBloodGroup := pet.BloodGroupName
 		bloodreq.MatchingDonors = append(bloodreq.MatchingDonors, bloodreqmodel.MatchingDonorReadModel{
 			PetName:         pet.Name,
