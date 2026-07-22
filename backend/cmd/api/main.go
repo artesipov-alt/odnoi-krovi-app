e8bb691f7f3a415228ea95801feb1889bbcd88ca
Ruslan <r.stupa@ya.ru>
2026-07-22 15:36:51 +0700

    fix(bloodsearch): adopt pet enricher in donor query

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -206,7 +206,7 @@ func main() {
 		bloodDeleteHandler := bloodcmd.NewDeleteRequestHandler(bloodRequestRepo, txManager)
 		bloodGetByIDHandler := bloodquery.NewGetByIDHandler(bloodRequestRepo, petRepo)
 		bloodGetByPetIDHandler := bloodquery.NewGetByPetIDHandler(bloodRequestRepo, petRepo, petEnricher)
-		bloodGetDonorByIDHandler := bloodquery.NewGetDonorByIDHandler(petRepo, donorResponseRepo, bloodRequestRepo)
+		bloodGetDonorByIDHandler := bloodquery.NewGetDonorByIDHandler(petRepo, donorResponseRepo, bloodRequestRepo, petEnricher)
 		bloodGetDonationHandler := bloodquery.NewGetDonationHandler(petRepo, donorResponseRepo, userRepo, bloodRequestRepo)
 		applyResponseHandler := bloodcmd.NewApplyResponseHandler(bloodRequestRepo, donorResponseRepo, petRepo, userRepo, publisher, txManager)
 		confirmDonationHandler := bloodcmd.NewConfirmDonationHandler(bloodRequestRepo, donorResponseRepo, petRepo, userRepo, txManager, publisher, bonusSvc)
--- backend/internal/application/bloodsearch/query/get_donor_by_id.go
+++ backend/internal/application/bloodsearch/query/get_donor_by_id.go
@@ -3,9 +3,9 @@ package query
 import (
 	"context"
 	"errors"
-	"time"
 
 	"github.com/artesipov-alt/odnoi-krovi-app/internal/apperrors"
+	"github.com/artesipov-alt/odnoi-krovi-app/internal/application/pet/enrich"
 	"github.com/artesipov-alt/odnoi-krovi-app/internal/domain/bloodsearch"
 	"github.com/artesipov-alt/odnoi-krovi-app/internal/domain/donor"
 	donormodel "github.com/artesipov-alt/odnoi-krovi-app/internal/domain/donor/model"
@@ -18,14 +18,16 @@ type GetDonorByIDHandler struct {
 	donorRepo    donor.Repository
 	bloodRepo    bloodsearch.Repository
 	bloodCounter *bloodsearch.BloodCounterService
+	enricher     enrich.PetEnricher
 }
 
-func NewGetDonorByIDHandler(petReadRepo pet.PetReadRepository, donorRepo donor.Repository, bloodRepo bloodsearch.Repository) *GetDonorByIDHandler {
+func NewGetDonorByIDHandler(petReadRepo pet.PetReadRepository, donorRepo donor.Repository, bloodRepo bloodsearch.Repository, enricher enrich.PetEnricher) *GetDonorByIDHandler {
 	return &GetDonorByIDHandler{
 		petReadRepo:  petReadRepo,
 		donorRepo:    donorRepo,
 		bloodRepo:    bloodRepo,
 		bloodCounter: bloodsearch.NewBloodCounterService(),
+		enricher:     enricher,
 	}
 }
 
@@ -35,27 +37,24 @@ func (h *GetDonorByIDHandler) Handle(ctx context.Context, petID string, opts pet
 		return nil, nil, err
 	}
 
+	fc, err := h.enricher.Fetch(ctx, []string{petID})
+	if err != nil {
+		return nil, nil, err
+	}
+
 	application, err := h.donorRepo.GetByPetID(ctx, petID)
 	if err != nil && !errors.Is(err, apperrors.ErrDonorResponseNotFound) {
 		return nil, nil, err
 	}
-	// if application == nil {
-	// 	return nil, nil, apperrors.ErrDonorResponseNotFound
-	// }
 
-	bloodReq, err := h.bloodRepo.GetByPetID(ctx, petID)
-	if err != nil && !errors.Is(err, apperrors.ErrBloodRequestNotFound) {
-		return nil, nil, err
-	}
-
-	if bloodReq != nil {
+	if bloodReq := fc.BloodReqs[petID]; bloodReq != nil {
 		donated, reserved := h.bloodCounter.RecalculateBloodAmount(bloodReq.BloodRequest, bloodReq.DonorApplications)
 
 		bloodReq.BloodRequest.SetBloodVolume(donated, reserved)
 		bloodReq.RecalculateStatus()
 	}
 
-	donorPet.RecalculateStatus(time.Now(), pet.BuildDonationContext(application, bloodReq))
+	h.enricher.Recalculate(donorPet, fc, enrich.Options{})
 
 	return donorPet, application, nil
 }
